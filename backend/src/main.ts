44965b1679eeb510b6218098011a74201cb16674
Ismail Ahmed <143304919+ismailAhmed0000@users.noreply.github.com>
2026-06-24 21:46:31 +0500

    cors set

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,5 +5,6 @@ import 'dotenv/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({ origin: 'http://localhost:5173' });
 }
 bootstrap();
